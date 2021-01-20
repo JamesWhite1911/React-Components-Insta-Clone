dbb8811092eae82f6e22597005bd5cdb6080d52c
James <JamesWhite1911@protonmail.com>
2021-01-19 21:20:12 -0500

    mvp complete

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
+import './dummy-data';
 import './App.css';
+import dummyData from './dummy-data';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  const [posts, setPosts] = useState(dummyData);
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
-    /*
+    setPosts(posts.map(post => {
+      return post.id === postId ? { ...post, likes: post.likes + 1 } : { ...post };
+    })
+    );
+        /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
@@ -32,6 +41,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts posts = { posts } likePost = { likePost } />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
